428203bcb1187e7f395be4096d507f2a97b87221
David Harris <davidaharris@outlook.com>
2022-12-05 22:05:24 -0500

    day5

---
+++ day5/5b.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import re
+from collections import defaultdict
+
+input = open('input.txt').read().split('\n\n')
+
+crates = input[0].splitlines()[:-1]
+instructions = input[1].splitlines()
+
+# Parse Stacks
+stacks = defaultdict(list)
+for line in crates:
+    split_crates = re.findall('....?', line)
+    for stack, crate in enumerate(split_crates):
+        if not crate.isspace():
+            stacks[stack + 1].append(crate[1])
+
+# Parse Instructions
+for line in instructions:
+    num, from_stack, to_stack = map(int, re.findall('\d+', line))
+    # stacks[from_stack], stacks[to_stack] = stacks[from_stack][num:],  stacks[from_stack][:num][::-1] + stacks[to_stack]
+    stacks[to_stack] = stacks[from_stack][:num] + stacks[to_stack]
+    stacks[from_stack] = stacks[from_stack][num:]
+    
+
+print("".join([stacks[key][0] for key in sorted(stacks)]))
+
+
+
+
